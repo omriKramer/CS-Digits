e0c99ef6363d028167224300cd6be0f7349297ba
Omri Kramer <omri.kramer@gmail.com>
2019-05-15 16:29:21 +0300

    Model Should be moved to device before creating the optimizer

--- points_net.py
+++ points_net.py
@@ -35,11 +35,11 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 num_classes = 10
 num_instructions = len(train.instructions)
 model = CounterStreamNet([2, 2, 2, 2], num_classes=num_classes, num_instructions=num_instructions)
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters())
 model.to(device)
+optimizer = optim.Adam(model.parameters())
 
 #%% Train the model
+criterion = nn.MSELoss()
 model, hist = train_model(model, loaders_dict, optimizer, criterion, device, num_epochs=2)
 
 #%% Alternatively load the model
@@ -47,40 +47,41 @@ checkpoint = torch.load('points/model_mse.tar', map_location=device)
 model.load_state_dict(checkpoint['model_state_dict'])
 optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-
 #%%
 data_iter = iter(loaders_dict['val'])
-data = data_iter.next()
-
 
 #%%
-model.clear()
-model.eval()
-with torch.no_grad():
-    bu1_output = model(data['image'], 'BU').sigmoid()
-    seg_out = model(data['instruction_idx'], 'TD')
+for i, data in enumerate(data_iter):
+    if i == 10:
+        break
+    model.clear()
+    model.eval()
+    with torch.no_grad():
+        bu1_output = model(data['image'], 'BU').sigmoid()
+        seg_out = model(data['instruction_idx'], 'TD')
 
-fig, axes = plt.subplots(len(seg_out), 2)
-for ax, img, gt, seg, instruction in zip(axes, data['image'], data['segmentation'], seg_out, data['instruction']):
-    img = img.numpy().transpose(1, 2, 0).squeeze()
-    gt = gt.numpy().transpose(1, 2, 0).squeeze()
-    seg = seg.numpy().transpose(1, 2, 0).squeeze()
-    seg = np.where(seg < 0.6, 0, 1)
+    fig, axes = plt.subplots(len(seg_out), 2)
+    for ax, img, gt, seg, instruction in zip(axes, data['image'], data['segmentation'], seg_out, data['instruction']):
+        img = img.numpy().transpose(1, 2, 0).squeeze()
+        gt = gt.numpy().transpose(1, 2, 0).squeeze()
+        seg = seg.numpy().transpose(1, 2, 0).squeeze()
+        seg = np.where(seg < 0.6, 0, 1)
 
-    ax[0].imshow(img, cmap='gray')
-    ax[0].imshow(gt, alpha=0.7, cmap='gray')
-    ax[0].set_ylabel(instruction)
-    ax[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+        ax[0].imshow(img, cmap='gray')
+        ax[0].imshow(gt, alpha=0.7, cmap='gray')
+        ax[0].set_ylabel(instruction)
 
-    ax[1].imshow(img, cmap='gray')
-    ax[1].imshow(seg, alpha=0.7, cmap='gray')
-    ax[1].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+        ax[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
 
-axes[0][0].set_title('Ground Truth')
-axes[0][1].set_title('Output')
-fig.tight_layout()
-plt.show()
+        ax[1].imshow(img, cmap='gray')
+        ax[1].imshow(seg, alpha=0.7, cmap='gray')
+        ax[1].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
 
+    axes[0][0].set_title('Ground Truth')
+    axes[0][1].set_title('Output')
+    fig.tight_layout()
+    # plt.show()
+    fig.savefig(f'../../Desktop/res{i}.png')
 #%%
 model.eval()
 results = []
